b30c39eb6118e3ff3f44952bcef73744bfea40ce
zhanglu <809740443@qq.com>
2020-09-07 00:12:42 +0800

    feat: 添加button测试

--- src/components/Button/__tests__/index.test.tsx
+++ src/components/Button/__tests__/index.test.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Button from '../index';
-import '@testing-library/jest-dom/extend-expect';
+import React from "react";
+import { render } from "@testing-library/react";
+import Button from "../index";
+import "@testing-library/jest-dom/extend-expect";
 
-test('button', () => {
-  const warpper = render(<Button>test</Button>);
-  const ele = warpper.getByText('test');
-  expect(ele).toBeInTheDocument()
-})
+describe("test button", () => {
+  it("render default button", () => {
+    const { getByText } = render(<Button>default</Button>);
+    const defaultButton = getByText("default");
+    expect(defaultButton).toHaveClass('picasso-btn picasso-btn-primary picasso-btn-middle')
+  });
+});
