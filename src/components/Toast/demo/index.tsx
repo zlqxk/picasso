8b09f694f4f9f897fc8ecb82438813134f591a03
zhanglu <809740443@qq.com>
2020-10-14 19:06:42 +0800

    update: 新增toast种类 🚀

--- src/components/Toast/demo/index.tsx
+++ src/components/Toast/demo/index.tsx
@@ -1,18 +1,58 @@
-import React from 'react';
-import Toast from '../index';
-import Button from '../../Button'
+import React from "react";
+import Toast from "../index";
+import Button from "../../Button";
 
 const Demo = () => {
   const info = () => {
-    Toast.info('系统错大神阿萨德阿萨德误', 2, () => {
-      console.log(1)
-    }, false)
-  }
+    const a = Toast.info("系统错大神阿萨德阿萨德误", 2, () => {
+      console.log(1);
+    });
+    console.log(a, 'a')
+  };
+  const success = () => {
+    Toast.success(
+      "请求成功",
+      2,
+      () => {
+        console.log(1);
+      },
+      false
+    );
+  };
+  const fail = () => {
+    Toast.fail(
+      "因为网络错误，您的请求失败，请稍后再试",
+      2,
+      () => {
+        console.log(1);
+      },
+      false
+    );
+  };
+
+  const loading = () => {
+    Toast.loading(
+      "正在加载",
+      -2,
+      () => {
+        console.log(1);
+      },
+      false
+    );
+  };
   return (
     <div>
       <Button onClick={info}>我是info</Button>
+      <br />
+      <Button onClick={success}>我是success</Button>
+      <br />
+      <Button onClick={fail}>我是fail</Button>
+      <br />
+      <Button onClick={loading}>我是Loading</Button>
+      <br/>
+      <Button onClick={Toast.hide}>list</Button>
     </div>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,42 +1,71 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import classNames from "classnames";
-interface ToastType {}
-
+import { Success, Fail, Loading } from "../Icon/index";
+const typeMap = {
+  success: <Success />,
+  fail: <Fail />,
+  info: null,
+  loading: <Loading />,
+};
+let toastList: Array<HTMLElement> = [];
+let timeoutList: Array<any> = [];
 const prefixCls = "picasso-toast";
 const classes = classNames(prefixCls);
+const body = document.getElementsByTagName("body")[0];
+
 const Toast = (
   content: React.ReactNode,
-  type: string,
+  type: "success" | "fail" | "info" | "loading",
   duration = 3,
-  onClose?: () => void,
-  mask = true
+  mask = true,
+  onClose?: () => void
 ) => {
-  const container = document.createElement("div");
-  const masker = document.createElement("div");
-  container.className = classes;
-  masker.className = `${prefixCls}-mask`;
-  const body = document.getElementsByTagName("body")[0];
+  const container = createElement("div", classes);
+  const masker = createElement("div", `${prefixCls}-mask`);
   // 声明挂载元素
-  const App: React.FC<ToastType> = () => {
-    return <div className={`${prefixCls}-content`}>{content}</div>;
+  const App: React.FC = () => {
+    return (
+      <div className={`${prefixCls}-content`}>
+        <div>{typeMap[type]}</div>
+        <div>{content}</div>
+      </div>
+    );
   };
-  mask && body.appendChild(masker);
+  if (mask) {
+    toastList.push(masker);
+    body.appendChild(masker);
+  }
+  toastList.push(container);
   body.appendChild(container);
   ReactDOM.render(<App />, container);
   setTimeout(() => {
     container.className = classNames(classes, "appear");
   });
-  if (duration < 0) return;
-  setTimeout(() => {
-    container.className = classes;
-    setTimeout(() => {
-      ReactDOM.unmountComponentAtNode(container);
-      mask && body.removeChild(masker);
-      body.removeChild(container);
-      onClose && onClose();
-    }, 200);
+  let timeoutId = setTimeout(() => {
+    if (duration >= 0) {
+      container.className = classes;
+      setTimeout(() => {
+        ReactDOM.unmountComponentAtNode(container);
+        if (mask) {
+          body.removeChild(masker);
+          toastList = toastList.filter(item => item !== masker);
+        }
+        body.removeChild(container);
+        toastList = toastList.filter(item => item !== container);
+        onClose && onClose();
+      }, 200);
+    }
   }, duration * 1000);
+  timeoutList.push(timeoutId);
+  // TODO 暂时返回null
+  return null;
+};
+
+const createElement = (type: string, className: string) => {
+  const elementDom = document.createElement(type);
+  elementDom.className = className;
+  return elementDom;
 };
 
 const info = (
@@ -45,7 +74,7 @@ const info = (
   onClose?: () => void,
   mask?: boolean
 ) => {
-  Toast(content, "info", duration, onClose, mask);
+  return Toast(content, "info", duration, mask, onClose);
 };
 
 const success = (
@@ -54,11 +83,42 @@ const success = (
   onClose?: () => void,
   mask?: boolean
 ) => {
-  Toast(content, "success", duration, onClose, mask);
+  return Toast(content, "success", duration, mask, onClose);
 };
 
+const fail = (
+  content: React.ReactNode,
+  duration?: number,
+  onClose?: () => void,
+  mask?: boolean
+) => {
+  return Toast(content, "fail", duration, mask, onClose);
+};
+
+const loading = (
+  content: React.ReactNode,
+  duration?: number,
+  onClose?: () => void,
+  mask?: boolean
+) => {
+  return Toast(content, "loading", duration, mask, onClose);
+};
+
+const hide = () => {
+  timeoutList.forEach(item => {
+    clearTimeout(item);
+  });
+  toastList.forEach(item => {
+    body.removeChild(item);
+  });
+  toastList = [];
+  // TODO 通过hide关闭暂时不支持执行onclose参数
+};
 
 export default {
   info,
-  success
+  success,
+  fail,
+  loading,
+  hide,
 };
